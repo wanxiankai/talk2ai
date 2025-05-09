1b3ca700164abde66a01a93d9fffd3f3a41989bd
wanxiankai <xiankai.wan@gmail.com>
2025-05-09 13:37:30 +0800

    feat: add next image config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: [
+      'avatars.githubusercontent.com',  // GitHub 头像
+      'lh3.googleusercontent.com',      // Google 头像
+      'platform-lookaside.fbsbx.com',   // Facebook 头像
+      'pbs.twimg.com',                  // Twitter 头像
+    ],
+  },
 };
 
 export default nextConfig;
