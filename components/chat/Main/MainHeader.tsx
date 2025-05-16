69e67984d9294ecb319bd908ff7a2fb6d5dea393
wanxiankai <xiankai.wan@gmail.com>
2025-05-16 13:55:58 +0800

    feat: update the main header component

--- components/chat/Main/MainHeader.tsx
+++ components/chat/Main/MainHeader.tsx
@@ -3,27 +3,19 @@ import Image from 'next/image'
 import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from '@/components/ui/dropdown-menu'
 import {
     LogOut,
-    Settings,
     UserRound,
-    BadgeDollarSign
 } from "lucide-react"
-import { useMetaMask } from '@/context/MetaMaskContext'
-import { toast } from 'react-toastify'
-import { useUserStore } from '@/store/userStore'
-import { formatAccountAddress } from '@/lib/formatAddress'
+import { signOut, useSession } from 'next-auth/react'
 
 export default function MainHeader() {
-    const { logout } = useMetaMask()
-    const wallet = useUserStore(state => state.wallet)
-
-    const copyAccount = () => {
-        window.navigator.clipboard.writeText(wallet.account)
-        toast.success('Copied to clipboard')
+    const { data: session } = useSession();
+    if (!session || !session.user) {
+        return null;
     }
 
-    const handleCharge = () => {
-        
-    }
+    const { user } = session
+    const userName = user.name || user.email || '用户'; 
+    const userImage = user.image || '/asserts/png/default-avatar.png'; 
 
     return (
         <div className='flex flex-row items-center justify-between pt-4 pl-8 pr-5'>
@@ -31,31 +23,17 @@ export default function MainHeader() {
             <div>
                 <DropdownMenu>
                     <DropdownMenuTrigger asChild>
-                        <Image src='/asserts/svg/default_avatar.svg' alt='user' width={36} height={36} className='rounded-full cursor-pointer' />
+                        <Image src={userImage} alt='user' width={36} height={36} className='rounded-full cursor-pointer' />
                     </DropdownMenuTrigger>
                     <DropdownMenuContent className="w-44">
                         <DropdownMenuGroup>
-                            <DropdownMenuItem onClick={copyAccount}>
-                                <UserRound className="mr-2 h-4 w-4" />
-                                <span>{formatAccountAddress(wallet.account)}</span>
-                            </DropdownMenuItem>
-                        </DropdownMenuGroup>
-                        <DropdownMenuSeparator />
-                        {/* <DropdownMenuGroup>
-                            <DropdownMenuItem onClick={handleCharge}>
-                                <BadgeDollarSign className="mr-2 h-4 w-4" />
-                                <span>Charge</span>
-                            </DropdownMenuItem>
-                        </DropdownMenuGroup>
-                        <DropdownMenuSeparator /> */}
-                        <DropdownMenuGroup>
                             <DropdownMenuItem>
-                                <Settings className="mr-2 h-4 w-4" />
-                                <span>Settings</span>
+                                <UserRound className="mr-2 h-4 w-4" />
+                                <span>{userName}</span>
                             </DropdownMenuItem>
                         </DropdownMenuGroup>
                         <DropdownMenuSeparator />
-                        <DropdownMenuItem onClick={logout}>
+                        <DropdownMenuItem onClick={() => signOut()}>
                             <LogOut className="mr-2 h-4 w-4" />
                             <span>Log out</span>
                         </DropdownMenuItem>
