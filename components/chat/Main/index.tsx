ab2172745ff59e11e54ad6ce7078858a063412c6
wanxiankai <xiankai.wan@gmail.com>
2025-05-21 23:03:26 +0800

    feat: init main

--- components/chat/Main/index.tsx
+++ components/chat/Main/index.tsx
@@ -7,17 +7,16 @@ import HelpIcon from './HelpIcon'
 import { useChatStore } from '@/store/chatStore'
 
 export default function Main() {
-    const sessionId = useUserStore(state => state.sessionId)
-    const fetchMessageList = useChatStore(state => state.fetchMessageList)
+    const { chatId, fetchMessageList } = useChatStore(state => state)
     useEffect(() => {
-        if (sessionId) {
+        if (chatId) {
             initMessageList()
         }
     }, [])
 
     const initMessageList = async () => {
-        if (sessionId) {
-            await fetchMessageList(sessionId)
+        if (chatId) {
+            await fetchMessageList(chatId)
         }
     }
 
@@ -25,7 +24,7 @@ export default function Main() {
         <div className="flex flex-col items-center justify-start flex-1 bg-[url('/asserts/png/chatbg.png')] bg-center bg-no-repeat bg-cover">
             <main className='flex flex-col w-full h-full overflow-y-auto relative'>
                 <MainHeader />
-                {!sessionId && <Welcome />}
+                {!chatId && <Welcome />}
                 <MessageList />
                 <ChatInput />
                 <HelpIcon />
