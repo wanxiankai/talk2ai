05d417030c2806fd3eec05b21bf334f5892fdd88
wanxiankai <xiankai.wan@gmail.com>
2025-05-21 23:03:52 +0800

    feat: add dialog

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -2,6 +2,7 @@
 
 import Main from '@/components/chat/Main'
 import Navigation from '@/components/chat/Navigation'
+import DeleteChatDialog from '@/components/dialog/DeleteChatDialog'
 import React from 'react'
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
     <div className="flex-1 flex flex-row w-full overflow-y-auto overflow-x-hidden">
       <Navigation />
       <Main />
+      <DeleteChatDialog />
     </div>
   )
 }
