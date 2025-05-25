fb2d3e289de2f3bb8af5779891ae95c38667ffce
wanxiankai <xiankai.wan@gmail.com>
2025-05-25 22:17:05 +0800

    feat: optimize the main

--- components/chat/Main/index.tsx
+++ components/chat/Main/index.tsx
@@ -7,7 +7,7 @@ import HelpIcon from './HelpIcon'
 import { useChatStore } from '@/store/chatStore'
 
 export default function Main() {
-    const { chatId, fetchMessageList } = useChatStore(state => state)
+    const { chatId, fetchMessageList, messageList } = useChatStore(state => state)
     useEffect(() => {
         if (chatId) {
             initMessageList()
@@ -24,7 +24,7 @@ export default function Main() {
         <div className="flex flex-col items-center justify-start flex-1 bg-[url('/asserts/png/chatbg.png')] bg-center bg-no-repeat bg-cover">
             <main className='flex flex-col w-full h-full overflow-y-auto relative'>
                 <MainHeader />
-                {!chatId && <Welcome />}
+                {messageList.length === 0 && <Welcome />}
                 <MessageList />
                 <ChatInput />
                 <HelpIcon />
