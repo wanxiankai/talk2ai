48e6e0649f13b8670030f28f9c0db18f07561c8c
wanxiankai <xiankai.wan@gmail.com>
2025-05-25 20:50:33 +0800

    feat: optimize the sidebar

--- components/chat/Navigation/index.tsx
+++ components/chat/Navigation/index.tsx
@@ -9,7 +9,7 @@ import NewChatButton from './NewChatButton'
 export default function Navigation() {
     const showFullNav = useAppStore((state) => state.showFullNav)
     return (
-        <nav className={classNames(showFullNav ? '' : '', 'min-w-72  h-full flex flex-col items-start bg-[#f9f9f9]')}>
+        <nav className={classNames(showFullNav ? 'min-w-72 ' : 'hidden', 'h-full flex flex-col items-start bg-[#f9f9f9]')}>
             <SearchBar />
             <ChatList />
             <NewChatButton />
