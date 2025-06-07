c470d3c68db758cd41ae9d4abb307c7e8c5ff1a1
wanxiankai <xiankai.wan@gmail.com>
2025-06-07 12:00:34 +0800

    feat: complete the welcome page

--- components/chat/Main/Welcome.tsx
+++ components/chat/Main/Welcome.tsx
@@ -8,40 +8,21 @@ export default function Welcome() {
             <div className='flex-none pl-8'>
                 <img src='/asserts/svg/welcome_title.svg' alt='chatbg' className='w-[603px] h-[65px]' />
             </div>
-            <div className='flex flex-row items-center justify-center mt-[100px] gap-4 text-sm font-normal'>
-                <div className='w-48 bg-white rounded-3xl pt-5 pb-2.5 px-3.5 flex flex-col items-center hover:-translate-y-2 transform transition-transform duration-200 ease-in-out hover:shadow-[0_4px_30px_0_rgba(0,0,0,0.06)]'>
+            <div className='flex flex-row items-center justify-center mt-[100px] gap-8 text-sm font-normal'>
+                <div className='w-48 min-h-38 bg-white rounded-3xl px-3.5 flex  items-center cursor-pointer hover:-translate-y-2 transform transition-transform duration-200 ease-in-out hover:shadow-[0_4px_30px_0_rgba(0,0,0,0.06)]'>
                     <span>
-                        Pay Ardio Labs members’ salaries, just like last month.
+                        T2ai is a personal AI chatbot project of talk to ai.
                     </span>
-                    <div className='w-full mt-[13px] flex items-center justify-between'>
-                        <img src='/asserts/png/uim_box.png' alt='bitcion' className='w-[42px] h-[42px]' />
-                    </div>
-
                 </div>
-                <div className='w-48 bg-white rounded-3xl pt-5 pb-2.5 px-3.5 flex flex-col items-center hover:-translate-y-2 transform transition-transform duration-200 ease-in-out hover:shadow-[0_4px_30px_0_rgba(0,0,0,0.06)]'>
+                <div className='w-48 min-h-38 bg-white rounded-3xl px-3.5 flex  items-center cursor-pointer hover:-translate-y-2 transform transition-transform duration-200 ease-in-out hover:shadow-[0_4px_30px_0_rgba(0,0,0,0.06)]'>
                     <span>
-                        What happened in crypto today?
+                        You can use it to ask questions about anything for free.
                     </span>
-                    <div className='w-full mt-[35px]'>
-                        <img src='/asserts/png/uim_hangouts.png' alt='bitcion' className='w-[42px] h-[42px]' />
-                    </div>
                 </div>
-                <div className='w-48 bg-white rounded-3xl pt-5 pb-2.5 px-3.5 flex flex-col items-center hover:-translate-y-2 transform transition-transform duration-200 ease-in-out hover:shadow-[0_4px_30px_0_rgba(0,0,0,0.06)]'>
-                    <span>Help me buy $100 worth
-                        of BTC.
+                <div className='w-48 min-h-38 bg-white rounded-3xl px-3.5 flex  items-center cursor-pointer hover:-translate-y-2 transform transition-transform duration-200 ease-in-out hover:shadow-[0_4px_30px_0_rgba(0,0,0,0.06)]'>
+                    <span>
+                        Please give a star on github to support me if you like it.
                     </span>
-                    <div className='w-full mt-[35px]'>
-                        <img src='/asserts/png/uim_bitcoin.png' alt='bitcion' className='w-[42px] h-[42px]' />
-                    </div>
-                </div>
-                <div className='w-48 bg-white rounded-3xl pt-5 pb-2.5 px-3.5 flex flex-col items-center hover:-translate-y-2 transform transition-transform duration-200 ease-in-out hover:shadow-[0_4px_30px_0_rgba(0,0,0,0.06)]'>
-                    <span>I want Ardio to be
-                        compatible with a new
-                        contract.
-                    </span>
-                    <div className='w-full mt-[13px] flex items-center justify-between'>
-                        <img src='/asserts/png/uim_chart.png' alt='bitcion' className='w-[42px] h-[42px]' />
-                    </div>
                 </div>
             </div>
         </div>
