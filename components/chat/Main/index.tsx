7f4601daeee3eedfaf7cd619a8a67a3712b91626
wanxiankai <xiankai.wan@gmail.com>
2025-06-11 11:29:52 +0800

    feat: optimize the message list init

--- components/chat/Main/index.tsx
+++ components/chat/Main/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useCallback, useEffect } from 'react'
 import MainHeader from './MainHeader'
 import Welcome from './Welcome'
 import MessageList from './MessageList'
@@ -8,17 +8,18 @@ import { useChatStore } from '@/store/chatStore'
 
 export default function Main() {
     const { chatId, fetchMessageList, messageList } = useChatStore(state => state)
+    const initMessageList = useCallback(async () => {
+        if (chatId) {
+            await fetchMessageList(chatId)
+        }
+    }, [chatId, fetchMessageList])
+
     useEffect(() => {
         if (chatId) {
             initMessageList()
         }
-    }, [])
+    }, [chatId, initMessageList])
 
-    const initMessageList = async () => {
-        if (chatId) {
-            await fetchMessageList(chatId)
-        }
-    }
 
     return (
         <div className="flex flex-col items-center justify-start flex-1 bg-[url('/asserts/png/chatbg.png')] bg-center bg-no-repeat bg-cover">
