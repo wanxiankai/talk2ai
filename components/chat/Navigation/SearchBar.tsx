fe9a1d139a033bd46dfc5988f617477d080fd8df
wanxiankai <xiankai.wan@gmail.com>
2025-06-13 10:09:54 +0800

    feat: complete the search functionality

--- components/chat/Navigation/SearchBar.tsx
+++ components/chat/Navigation/SearchBar.tsx
@@ -1,11 +1,34 @@
 'use client'
+import { INITCHATLISTPAGE } from '@/constant/events'
+import eventBus from '@/lib/EventBus'
 import { useAppStore } from '@/store/appStore'
+import { useChatStore } from '@/store/chatStore'
+import { useDebounce } from 'ahooks'
 import Image from 'next/image'
-import React, { useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 
 export default function SearchBar() {
     const setShowFullNav = useAppStore((state) => state.setShowFullNav)
+    const { searchChatHistory, setIsSearching } = useChatStore((state) => state)
     const [keyword, setKeyword] = useState('')
+    const debouncedValue = useDebounce(keyword, { wait: 800 });
+
+    const searchChat = useCallback(async (keyword: string) => {
+        await searchChatHistory(keyword)
+    }, [searchChatHistory])
+
+    useEffect(() => {
+        const trimedValue = debouncedValue.trim()
+        if (trimedValue) {
+            setIsSearching(true)
+            searchChat(trimedValue)
+        } else {
+            setIsSearching(false)
+            eventBus.emit(INITCHATLISTPAGE)
+        }
+    }, [debouncedValue, searchChat, setIsSearching])
+
+
 
     return (
         <div className='w-full pl-[20px] flex flex-col items-start mb-12 pt-[20px] pr-[20px]'>
